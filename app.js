6dfc0b61dd40637224019d43ca4aeacec11b5e87
mandar-01 <bhaleraomandar01@gmail.com>
2016-12-24 21:53:13 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Mongo related code
+var mongoose = require('mongoose');
+require('./models/posts');
+mongoose.connect('mongodb://localhost/students');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static('public/javascripts'));
+
+// app.use('/', index.ejs);
+app.get('/', function(req, res){
+	console.log(req.params);
+	res.render("index");
+});
+app.use('/users', users);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.get("/about", function(req, res){
+	res.send("About page");
+});
+
+
+/*app.listen("3000", function(){
+	console.log("Listening");
+});*/
+
+
+
+module.exports = app;
